9404d1f1329d55ea1ee9c1f87aebb6f004e40857
AK-shat-JAIN <your-email@example.com>
2025-02-28 10:57:23 +0530

    removing solidqueue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,10 +49,9 @@ Rails.application.configure do
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
 
+  config.active_storage.analyzers = []
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
-  config.active_storage.analyze_performed = false
+  config.active_job.queue_adapter = :async
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
